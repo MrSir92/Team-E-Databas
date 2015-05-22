d2c349153bbd1d5f77cd622019353550610fcdbd
pansarljud <emil.lind89@gmail.com>
2015-05-22 15:45:12 +0200

    update db

--- database/webbservice/todoapp/urls.py
+++ database/webbservice/todoapp/urls.py
@@ -6,10 +6,16 @@ from todoapp import views
 urlpatterns = [
     # url(r'^tasks/$', views.TaskList.as_view()),
     # url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view()),
-    url(r'^lists/$', views.ListList.as_view()),
-    url(r'^lists/(?P<pk>[0-9]+)/$', views.ListDetail.as_view()),
-    url(r'^lists/(?P<pk>[0-9]+)/tasks/$', views.ListTasks.as_view()),
-    url(r'^lists/(?P<list_pk>[0-9]+)/tasks/(?P<pk>[0-9]+)/$', views.ListTaskDetail.as_view()),
+    url(r'^needs/$', views.ListNeed.as_view()),
+    url(r'^offers/$', views.ListOffer.as_view()),
+    url(r'^users/$', views.CreateUsers.as_view()),
+    url(r'^needs/(?P<pk>[0-9]+)/$', views.NeedDetail.as_view()),
+    url(r'^offers/(?P<pk>[0-9]+)/$', views.OfferDetail.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/needs/$', views.ListUserNeed.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/offers/$', views.ListUserOffer.as_view()),
+    url(r'^needs/(?P<list_pk>[0-9]+[a-z])/$', views.ListNeedCategory.as_view()),
+    url(r'^offers/(?P<list_pk>[0-9]+[a-z])/$', views.ListOfferCategory.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
