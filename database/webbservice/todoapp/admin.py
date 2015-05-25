b21081d90b072b9a57ca09f08da74902e4c735e4
pansarljud <emil.lind89@gmail.com>
2015-05-25 09:08:55 +0200

    db

--- database/webbservice/todoapp/admin.py
+++ database/webbservice/todoapp/admin.py
@@ -1,22 +1,27 @@
 from django.contrib import admin
 from todoapp.models import Offer, Need, User
 
-class UserInline(admin.StackedInline):
+class UserInline(admin.TabularInline):
     model = User
-    verbose_name = 'User'
-    can_delete = True
+    extra = 1
 
 class OfferAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     search_fields = ['title', 'created_at']
-    inlines = [UserInline]
+    """inlines = [UserInline]"""
     verbose_name = 'title'
 
 class NeedAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     search_fields = ['title', 'created_at']
-    inlines = [UserInline]
-    verbose_name = 'title'    
+    """inlines = [UserInline]"""
+    verbose_name = 'title'
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ['name', 'created_at']
+    verbose_name = 'name'    
     
 admin.site.register(Offer, OfferAdmin)
 admin.site.register(Need, NeedAdmin)
+admin.site.register(User, UserAdmin)
